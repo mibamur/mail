4c8ea696ba7bb2f0491686a41caa8b6a5806da76
Michael <mibamur@gmail.com>
2016-06-09 12:34:12 +0900

    value.force_encoding("UTF-8").ascii_only?

--- lib/mail/fields/common/parameter_hash.rb
+++ lib/mail/fields/common/parameter_hash.rb
@@ -43,7 +43,8 @@ module Mail
     def encoded
       map.sort_by { |a| a.first.to_s }.map! do |key_name, value|
         # unless value.ascii_only?
-        unless value.blank?
+        # unless value.blank?
+        unless value.force_encoding("UTF-8").ascii_only?
           value = Mail::Encodings.param_encode(value)
           key_name = "#{key_name}*"
         end
